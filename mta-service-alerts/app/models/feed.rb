f535d08c3adf1422e87d80fb00e5b5ba0ad2a007
hbaughman <hbaughman741@gmail.com>
2015-03-15 13:41:18 -0400

    Refactor page fetch into feed

--- mta-service-alerts/app/models/feed.rb
+++ mta-service-alerts/app/models/feed.rb
@@ -1,23 +1,22 @@
+require 'open-uri'
+
 class Feed
 
   def initialize
-
+    page = get_page
+    doc = parse_page page
   end
 
-  def get_lines doc
-    
-  end
 
   def get_page
-
+    url = "http://web.mta.info/status/serviceStatus.txt"
+    open url
   end
 
   def parse_page page
-
+    Nokogiri::XML page
   end
 
-  def extract_data line
 
-  end
 
 end
